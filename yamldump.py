51b229abbb98f1b3bbd37f588ccf5a1578d03d7c
Sai Varun <53481276+mylifeasvarun@users.noreply.github.com>
2022-05-30 22:46:13 +0530

    Update yamldump.py

--- yamldump.py
+++ yamldump.py
@@ -1,8 +1,8 @@
+#Conversion of json format to yaml format
 from yaml.resolver import BaseResolver
 import yaml
+#importing data from runn
 from runn import para as data
-#print("*********")
-#print(data)
 class AsLiteral(str):
     pass
 
@@ -11,15 +11,17 @@ def represent_literal(dumper, data):
         BaseResolver.DEFAULT_SCALAR_TAG, data, style="|"
     )
 
-
+#Writing the data into nlu.yml 
 def create_yml(path, data):
     with open(path, "w") as ymls:
+        #converting data into yaml format
         yaml.dump(data, ymls, default_flow_style=False, sort_keys=False, width=700)
 
 
 import json 
 
-yaml.add_representer(AsLiteral, represent_literal)        
+yaml.add_representer(AsLiteral, represent_literal)    
+#preparing of data    
 def toformat(data):
     temp=[]
     for each in data:  
